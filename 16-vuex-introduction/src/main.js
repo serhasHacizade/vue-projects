b6b9da5b0300e23d0b17fa25068472e0c80471bc
serhasHacizade <eren.felek.4@gmail.com>
2023-10-28 13:21:11 +0300

    commit

--- 16-vuex-introduction/src/main.js
+++ 16-vuex-introduction/src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import store from "./store/store.js"
+import store from "./store.js"
 
 const app = createApp(App);
 app.use(store);
 app.mount('#app');
+
