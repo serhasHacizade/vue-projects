16ce8d789d94cf89d5cb5887f8d0ec49a3cace1c
serhasHacizade <eren.felek.4@gmail.com>
2023-12-02 12:19:06 +0300

    commit

---
+++ 20-socket.io/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script>
+  import ChatApp from './components/ChatApp.vue';
+  export default {
+    name: "App",
+    components: {
+      ChatApp
+    }
+  }
+</script>
+
+<template>
+    <ChatApp></ChatApp>
+</template>
---
+++ 20-socket.io/frontend/src/components/ChatApp.vue
@@ -0,0 +1,80 @@
+<script>
+export default {
+    data() {
+        return {
+            joined: false,
+            currentUser: ""
+        }
+    },
+    methods: {
+        join() {
+            this.joined = true
+            console.log(this.currentUser);
+        },
+        sendMessage() {
+            console.log(this.text);
+        }
+    },
+    name: "ChatApp",
+    text: ""
+}
+</script>
+
+<template>
+    <div v-if="!joined" class="parent-container">
+        <div class="name-container">
+            <input v-model="currentUser" type="text" class="user-name" />
+            <button v-on:click="join" class="join-button">Join</button>
+        </div>
+    </div>
+    <div class="text-input-container">
+        <div v-if="joined">
+            <div>
+                <textarea v-model="text" class="text-message" v-on:keyup.enter="sendMessage"></textarea>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.parent-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    padding-top: 150px;
+}
+
+.name-container {
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+}
+
+.user-name {
+    height: 30px;
+    font-size: 20px;
+    padding: 5px;
+    margin-bottom: 5px;
+    text-align: center;
+    font-weight: bold;
+}
+
+.join-button {
+    height: 30px;
+    font-size: 15px;
+}
+.text-input-container {
+    height: 100vh;
+}
+.text-message {
+    width: 100%;
+    position: absolute;
+    bottom: 0px;
+    height: 70px;
+    padding: 10px;
+}
+
+
+</style>
