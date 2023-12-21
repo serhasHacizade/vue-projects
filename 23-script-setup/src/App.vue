562aafd7729807543ef3211fa389a9a044568f9a
serhasHacizade <eren.felek.4@gmail.com>
2023-12-21 16:33:31 +0300

    commit

--- 23-script-setup/src/App.vue
+++ 23-script-setup/src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
+import { ref } from 'vue';
+
+import oddOrEven from "./components/oddOrEven.vue";
+  const title = ref("Başlık");
+const counter = ref(0);
+
+const inc = () => counter.value++;
+
 
 </script>
 
 <template>
-  <h3>{{title}}</h3>
+  <h3>{{ title }}</h3>
+  <input type="text" v-model="title">
+  <button @click="inc">{{ counter }}</button>
+  <hr>
+  <oddOrEven :counter="counter" />
 </template>
---
+++ 23-script-setup/src/components/oddOrEven.vue
@@ -0,0 +1,8 @@
+<script setup>
+const props = defineProps({ counter: Number });
+</script>
+
+<template>
+    <h1>aaaaa</h1>
+    <h3>{{ props.counter }}</h3>
+</template>
