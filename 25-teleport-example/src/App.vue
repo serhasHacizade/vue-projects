bd3cb83e99d1e3162c7b883fb0c05c9baee07e76
serhasHacizade <eren.felek.4@gmail.com>
2024-01-08 09:25:46 +0300

    commit

---
+++ 25-teleport-example/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+
+  <teleport to='#modal-area'>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+  </teleport>
+
+  <teleport to='#modal-area'>
+    <h1>Selam</h1>
+  </teleport>
+</template>
---
+++ 26-namespaced-components/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import * as Form from "./components/form-components"
+</script>
+
+<template>
+  <Form.Label>
+    Başlık
+    <Input />
+  </Form.Label>
+  <Form.Label>
+    Kategori
+    <Form.Select />
+  </Form.Label>
+</template>
